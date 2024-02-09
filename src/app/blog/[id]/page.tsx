1d8a6d8fda3593d3029561588a733ee66e425ca6
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2024-02-09 20:19:14 +0300

    777

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,39 +1,35 @@
 import type { Metadata } from "next";
- import {use} from "react";
 import styles from "../../page.module.css";
-import { log } from "console";
 
-export function getData (id:string) {   
-    const data = use( fetch(`https://jsonplaceholder.typicode.com/posts/${id}`,{
+async function getData (id: string) {
+    const data = await fetch('https://jsonplaceholder.typicode.com/posts/' + id,{
       next: {
         revalidate: 60
       }
-    }).then(
-      response => response.json()
-    ));
-    return data
+    });
+    const  result = data.json();
+    return result;
   }
-
 type Props = {
     params: {
         id: string
     }
 }
+ export async function generateMetadata ({params: {id}}:Props): Promise<Metadata> {
+     const post = await getData(id)
+     return {
+        title: post.title,
+        description: post.body,
+   }
+}
 
-//  export async function generateMetadata ({params: {id}}:Props): Promise<Metadata> {
-//      const post = await getData(id)
-//      return {
-//         title: post.title,
-//         description: post.body,
-//    }
-// }
-
-export default  function Post({params: {id}}:Props) {    
-    const post = getData(id)
+export default async function Post  ({params}:Props) {    
+    const post = await getData(params.id)
 return (
     <main className={styles.main}>
-    <h1>post №{post.id}: {post.title}</h1>
+    <h1>post №{post.id}: {post.title}</h1>   
     <p>{post.body}</p>
     </main>
 )
 }
+
